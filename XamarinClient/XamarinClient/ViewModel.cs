e22ade32786b43f2ece09e29e706b42ecc51e594
DmitrievDV <13@13.tt>
2021-02-17 12:04:49 +0300

    add XamarinClient project

---
+++ XamarinClient/XamarinClient/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinClient
+{
+    class ViewModel
+    {
+        Color _selectedColor;
+        public Color SelectedColor
+        {
+            get => _selectedColor;
+            set { _selectedColor = value; BroadCastColor(value); }
+        }
+
+        void BroadCastColor(Color color)
+        {
+            var bytes = Encoding.ASCII.GetBytes(color.ToHex());
+            using var client = new UdpClient();
+            client.EnableBroadcast = true;
+            var endpoint = new IPEndPoint(IPAddress.Parse("192.168.43.255"), 4444);
+            client.Send(bytes, bytes.Length, endpoint);
+            client.Close();
+        }
+    }
+}
