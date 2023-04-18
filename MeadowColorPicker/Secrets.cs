732bde005974cb072eea907484e042f7557d37ce
Dblm0 <dmitrievdmitriymsk@gmail.com>
2023-04-18 16:00:10 +0300

    Add project files.

---
+++ MeadowColorPicker/Secrets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeadowColorPicker
+{
+    public class Secrets
+    {
+        /// <summary>
+        /// Name of the WiFi network to use.
+        /// </summary>
+        public const string WIFI_NAME = "[WIFI_NAME]";
+
+        /// <summary>
+        /// Password for the WiFi network names in WIFI_NAME.
+        /// </summary>
+        public const string WIFI_PASSWORD = "[WIFI_PASSWORD]";
+    }
+}
